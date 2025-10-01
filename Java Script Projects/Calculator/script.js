dc56345b22dd9970d43a2eb86c6393287d45dda6
Shoaib Amjad <shoaibamjaddev@gmail.com>
2025-09-30 21:24:14 -0700

    Updated Code

--- Java Script Projects/Calculator/script.js
+++ Java Script Projects/Calculator/script.js
@@ -2,11 +2,19 @@ function add(num){
 var input = document.getElementById("output");
 input.value += num;
 
-    console.log(num)
 }
 
 
 function equals(){
     var input = document.getElementById("output");
     input.value = eval(input.value);
+}
+
+function Del(){
+    var input = document.getElementById("output");
+    input.value = input.value.slice(0, -1);
+}
+function AC(){
+    var input = document.getElementById("output");
+    input.value = "";
 }
